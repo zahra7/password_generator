da345e68fcbe8c3c62d81463ebd37159cd9318cf
filalifz7 <filalifz7@gmail.com>
2022-01-02 18:21:14 +0100

    version 1

--- password_generator.py
+++ password_generator.py
@@ -5,7 +5,19 @@ def gen():
     upperString = string.ascii_uppercase
     lowerString = string.ascii_lowercase
     digitString = string.digits
-    print(string1)
-    print(string2)
-     
+    symbolString = string.punctuation
+    
+    passlen = int(input("Enter the password length : "))
+    
+    stringList = []
+    stringList.extend(list(upperString))
+    stringList.extend(list(lowerString))
+    stringList.extend(list(digitString))
+    stringList.extend(list(symbolString))
+    
+    random.shuffle(stringList)
+    
+    password = ("".join(stringList[0:passlen]))
+    print('The password is : ' , password)
+
 gen()
