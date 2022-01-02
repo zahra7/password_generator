86f0b20b889c322cdf10cf3cd07125e6d5f2f1bb
filalifz7 <filalifz7@gmail.com>
2022-01-02 18:11:46 +0100

    first commit

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+def gen():
+    upperString = string.ascii_uppercase
+    lowerString = string.ascii_lowercase
+    digitString = string.digits
+    print(string1)
+    print(string2)
+     
+gen()
